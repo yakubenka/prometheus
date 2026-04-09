cb11e7404552fc954322385c48da00ff6607cf99
yakubenka <63866789+yakubenka@users.noreply.github.com>
2026-04-08 23:42:30 -0400

    Add files via upload

--- set_allowances.py
+++ set_allowances.py
@@ -76,7 +76,7 @@ def main():
     try:
         from web3 import Web3
 
-        w3 = Web3(Web3.HTTPProvider("https://polygon-rpc.com"))
+        w3 = Web3(Web3.HTTPProvider("https://polygon-bor-rpc.publicnode.com"))
         account = w3.eth.account.from_key(private_key)
         addr    = account.address
         print(f"Wallet: {addr}")
